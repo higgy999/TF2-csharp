01cf5fd7d4ed48901e2b1f3fdad6b95fa5d4491b
Litl Toast <joshuafhiggins@gmail.com>
2021-05-06 16:41:21 -0400

    Commiting raylib version to master. gonna move it to a separate branch

---
+++ tf2ray/Player.cs
@@ -0,0 +1,60 @@
+﻿using Jitter;
+using Jitter.Collision.Shapes;
+using Jitter.Dynamics;
+using Jitter.LinearMath;
+using System.Numerics;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+
+namespace tf2ray
+{
+	public class Player
+	{
+		public RigidBody body;
+		public Shape shape;
+
+		public Player(ref World world, JVector position)
+		{
+			shape = new CylinderShape(10, 5);
+			body = new RigidBody(shape);
+			world.AddBody(body);
+			body.Position = position;
+		}
+
+		public void Update()
+		{
+			//reading the input:
+			float horizontalAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+			float verticalAxis = CrossPlatformInputManager.GetAxis("Vertical");
+
+			//assuming we only using the single camera:
+			var camera = Program.camera;
+
+			//camera forward and right vectors:
+			var forward = camera.target;
+			var right = camera.transform.right;
+
+			//project forward and right vectors on the horizontal plane (y = 0)
+			forward.y = 0f;
+			right.y = 0f;
+			forward.Normalize();
+			right.Normalize();
+
+			//this is the direction in the world space we want to move:
+			var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+
+			//now we can apply the movement:
+			body.Position = new JVector(desiredMoveDirection * 2 * GetFrameTime());
+	}
+
+		public void Render()
+		{
+			//Draw Player
+			Vector3 tmp = Program.ConvertJVector(body.Position);
+			tmp.Y -= 5f;
+
+			DrawCylinder(	  tmp, 5, 5, 10, 40, BLUE);
+			DrawCylinderWires(tmp, 5, 5, 10, 40, DARKBLUE);
+		}
+	}
+}
---
+++ tf2ray/Program.cs
@@ -0,0 +1,98 @@
+﻿using Raylib_cs;
+using static Raylib_cs.KeyboardKey;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.Color;
+using System.Numerics;
+using Jitter.Collision;
+using Jitter;
+using Jitter.Collision.Shapes;
+using Jitter.Dynamics;
+using Jitter.LinearMath;
+
+namespace tf2ray
+{
+	public class Program
+	{
+		static CollisionSystem collision;
+		static World world;
+
+		static Player player;
+
+		static RigidBody platformBody;
+		static Shape platformShape;
+
+		public static Camera3D camera;
+		static int windowWidth = 1280;
+		static int windowHeight = 720;
+
+		public static void Main()
+		{
+			InitWindow(windowWidth, windowHeight, "Team Fortress 2 (RayLib)");
+			SetTargetFPS(60);
+
+			collision = new CollisionSystemSAP();
+			world = new World(collision);
+
+			player = new Player(ref world, new JVector(0, 5, 0));
+
+			platformShape = new BoxShape(100, 1, 100);
+			platformBody = new RigidBody(platformShape);
+			world.AddBody(platformBody);
+			platformBody.AffectedByGravity = false;
+			platformBody.IsStatic = true;
+
+			Image ico = LoadImage(System.AppDomain.CurrentDomain.BaseDirectory + "/Resources/ico.png");
+			SetWindowIcon(ico);
+
+			camera = new Camera3D(new Vector3(0f, 10f, 0f), new Vector3(0f, 0f, 0f), new Vector3(0f, 1f, 0f), 90f, CameraType.CAMERA_PERSPECTIVE);
+
+			SetCameraMode(camera, CameraMode.CAMERA_FREE);
+
+			while (!WindowShouldClose())
+			{
+				Update();
+
+				BeginDrawing();
+				ClearBackground(RAYWHITE);
+				
+				BeginMode3D(camera);
+				Draw3D();
+				EndMode3D();
+
+				DrawUI();
+				EndDrawing();
+			}
+
+			CloseWindow();
+			UnloadImage(ico);
+		}
+
+		static void Update()
+		{
+			world.Step(1.0f / 60.0f, true);
+			UpdateCamera(ref camera);
+		}
+
+		static void Draw3D()
+		{
+			player.Render();
+
+			//Draw Platform
+			DrawCube(ConvertJVector(platformBody.Position), 100.0f, 1.0f, 100.0f, DARKGRAY);
+			DrawCubeWires(ConvertJVector(platformBody.Position), 100.0f, 1.0f, 100.0f, BLACK);
+
+			DrawGrid(200, 1.0f);
+		}
+
+		static void DrawUI()
+		{
+			DrawText("Welcome to the third dimension!", 10, 40, 20, DARKGRAY);
+			DrawFPS(10, 10);
+		}
+
+		public static Vector3 ConvertJVector(JVector input)
+		{
+			return new Vector3(input.X, input.Y, input.Z);
+		}
+	}
+}
