700b46f350285adafbcc327a50f6266838803c75
Litl Toast <joshuafhiggins@gmail.com>
2021-05-08 17:39:22 -0400

    all work no triangle

--- src/me/toast/tf2gl/Main.java
+++ src/me/toast/tf2gl/Main.java
@@ -1,10 +1,18 @@
 package me.toast.tf2gl;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
+import org.lwjgl.system.MemoryUtil;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL33.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Main {
@@ -68,13 +76,95 @@ public class Main {
         // bindings available for use.
         GL.createCapabilities();
 
+        //Set the camera viewport
+        glViewport(0, 0, 1280, 720);
+
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        //Set Shaders
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, loadAsString("./assets/shaders/triangle.vert"));
+        glCompileShader(vertexShader);
+        if (glGetShaderi(vertexShader, GL_COMPILE_STATUS) == GL_FALSE) {
+            System.err.println("Vertex Shader: " + glGetShaderInfoLog(vertexShader));
+            return;
+        }
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(vertexShader, loadAsString("./assets/shaders/triangle.frag"));
+        glCompileShader(fragmentShader);
+        if (GL20.glGetShaderi(fragmentShader, GL_COMPILE_STATUS) == GL_FALSE) {
+            System.err.println("Fragment Shader: " + glGetShaderInfoLog(fragmentShader));
+            return;
+        }
+
+        int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+
+        glLinkProgram(shaderProgram);
+        if (glGetProgrami(shaderProgram, GL_LINK_STATUS) == GL_FALSE) {
+            System.err.println("Program Linking: " + glGetProgramInfoLog(shaderProgram));
+            return;
+        }
+
+        glValidateProgram(shaderProgram);
+        if (glGetProgrami(shaderProgram, GL_VALIDATE_STATUS) == GL_FALSE) {
+            System.err.println("Program Validation: " + glGetProgramInfoLog(shaderProgram));
+            return;
+        }
+
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glEnableVertexAttribArray(0);
+
+        int VBO, VAO, EBO;
+
+        VAO = glGenVertexArrays();
+        VBO = glGenBuffers();
+        EBO = glGenBuffers();
+
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(VAO);
+
+        FloatBuffer positionBuffer = MemoryUtil.memAllocFloat(vertices.length);
+        positionBuffer.put(vertices).flip();
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, positionBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        IntBuffer indicesBuffer = MemoryUtil.memAllocInt(indices.length);
+        indicesBuffer.put(indices).flip();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        glEnableVertexAttribArray(0);
+
+        // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+        // remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        glBindVertexArray(0);
+
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( !glfwWindowShouldClose(window) ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+
+            // draw our first triangle
+            glUseProgram(shaderProgram);
+            glBindVertexArray(VAO);
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
             glfwSwapBuffers(window); // swap the color buffers
 
@@ -82,7 +172,36 @@ public class Main {
             // invoked during this call.
             glfwPollEvents();
         }
+        glDeleteVertexArrays(VAO);
+        glDeleteBuffers(VBO);
+        glDeleteBuffers(EBO);
+        glDeleteProgram(shaderProgram);
     }
+
+    public static String loadAsString(String location) {
+        StringBuilder result = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(location));
+            String buffer = "";
+            while ((buffer = reader.readLine()) != null) {
+                result.append(buffer);
+                result.append("\n");
+            }
+            reader.close();
+        } catch (IOException e) { System.err.println(e); }
+        return result.toString();
+    }
+
+    float vertices[] = {
+            0.5f,  0.5f, 0.0f,  // top right
+            0.5f, -0.5f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f   // top left
+    };
+    int indices[] = {  // note that we start from 0!
+            0, 1, 3,  // first Triangle
+            1, 2, 3   // second Triangle
+    };
 
     public static void main(String[] args) {
         new Main().run();
